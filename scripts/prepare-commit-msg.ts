be7bebc5986c82d588745669f0349d553a219c68
rhyei <298654965+rhyei@users.noreply.github.com>
2026-07-10 11:55:33 +0000

    ✨ chore

--- scripts/prepare-commit-msg.ts
+++ scripts/prepare-commit-msg.ts
@@ -3,11 +3,10 @@ import fs from 'fs'
 const commitMsgPath = process.argv[2]
 if (!commitMsgPath) process.exit(0)
 
-const emojiString = '✨🚀🐛🔥🎨⚡📝🔒♻️🐳🛠️📦🧪🚨🧹'
+// oxfmt-ignore
+const emojis = ['✨', '🌸', '🗻', '🤍', '🍥', '💎', '🎉', '🏮', '💥', '👾', '🐉', '🔥', '⚜️', '⛩️', '🥀']
 
 const currentMessage = fs.readFileSync(commitMsgPath, 'utf8')
-
-const emojis = Array.from(emojiString)
 const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)]
 const newMessage = `${randomEmoji} ${currentMessage}`
 
